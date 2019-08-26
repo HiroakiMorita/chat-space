bb322665811be72b7ad79fa3b0c5a9ea15b51957
HiroakiMorita <taiyou3291@outlook.jp>
2019-08-26 13:33:24 +0900

    インクリメンタルサーチの実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -23,6 +23,8 @@ $(function(){
     </div>`
     return html;
   }
+
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -51,3 +53,81 @@ $(function(){
     })
   })
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+// function addUser(userId,userName) {
+//   var html = `<div id='chat-group-users'>
+//                 <div class='chat-group-user clearfix js-chat-member' id='${userId}'>
+//                   <input name='group[user_ids][]' type='hidden' value='${userId}'>
+//                     <p class='chat-group-user__name'>${userName}</p>
+//                     <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+//               </div>`;
+//   member_list.append(html);
+//     }
+
+
+
+
+// $("#user-search-field").on("keyup", function() {
+//   var input = $("#user-search-field").val();
+//   $.ajax({
+//     type: 'GET',
+//     url: '/users',
+//     data: { keyword: input },
+//     dataType: 'json'
+//   })
+//   .done(function(users){
+//     $("#user-search-result").empty();
+//     if (users.length !== 0) {
+//       users.forEach(function(user){
+//         appendUser(user);
+//       });
+//     }
+//     else {
+//       appendNoUser("一致するユーザはいません");
+//     }
+//   })
+//   .fail(function(){
+//     alert('検索に失敗しました');
+//   })
+// });
+
+
+// var search_list = $("#user-search-result");
+
+// function appendUser(user_name){
+//   var html = `<div class="chat-group-user clearfix">
+//                 <p class="chat-group-user__name">
+//                 ${message.user_name}
+//                 </p>
+//                 <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id=${id} data-user-name=${user_name}>追加</a>
+//               </div>`;
+//     search_list.append(html);
+// }
